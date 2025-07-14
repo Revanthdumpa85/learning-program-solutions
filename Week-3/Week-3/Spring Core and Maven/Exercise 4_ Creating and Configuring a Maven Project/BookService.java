a71b4a2eb7f0eb1a4a5d23ca3a3e5d25c355f229
Revanthdumpa85 <dumparevanth8565@gmail.com>
2025-07-14 10:14:33 +0530

    Week 4 submitting

--- Week-3/Week-3/Spring Core and Maven/Exercise 4_ Creating and Configuring a Maven Project/BookService.java
+++
@@ -1,7 +0,0 @@
-package com.library.service;
-
-public class BookService {
-    public void welcomeMessage() {
-        System.out.println("LibraryManagement project initialized with Spring successfully!");
-    }
-}
--- Week-3/Week-3/Spring Data JPA with Spring Boot, Hibernate/Difference between JPA, Hibernate and Spring Data JPA/Hibernate Example/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Employee emp = new Employee();
-        emp.setName("John Doe");
-
-        EmployeeDAO dao = new EmployeeDAO();
-        dao.addEmployee(emp);
-    }
-}
